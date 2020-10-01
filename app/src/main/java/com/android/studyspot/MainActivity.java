c1b18da14417e1300de99094030e890a6dd42bc9
BrysonJames5 <40035530+BrysonJames5@users.noreply.github.com>
2020-10-01 08:26:23 -0400

    dependency corrections
    fixed dependencies and top navbar display

--- app/src/main/java/com/android/studyspot/MainActivity.java
+++ app/src/main/java/com/android/studyspot/MainActivity.java
@@ -3,29 +3,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 import android.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
 
+
 public class MainActivity
         extends AppCompatActivity {
-    TabLayout tabLayout;
-    ViewPageAdapter viewPageAdapter;
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         TabLayout.Tab map_fragment = tabLayout.newTab();
         TabLayout.Tab list_fragment = tabLayout.newTab();
         tabLayout.addTab(map_fragment, true);
         tabLayout.addTab(list_fragment);
         map_fragment.setText("Map");
         list_fragment.setText("Location List");
+
+
+    }
+
 
 
 

82cbb12875b5ba7ec6cb52439d9b9a2c67b184de
BrysonJames5 <40035530+BrysonJames5@users.noreply.github.com>
2020-10-01 15:47:49 -0400

    added OSU locations to Address object
    It is currently an array but am using Array.asList() to find String equivalents in the list

--- app/src/main/java/com/android/studyspot/MapFragment.java
+++ app/src/main/java/com/android/studyspot/MapFragment.java
@@ -49,10 +49,7 @@ public class MapFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override

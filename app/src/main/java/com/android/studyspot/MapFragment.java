06d995ec255b030d9d084d6f86907b518c2d2506
BrysonJames5 <40035530+BrysonJames5@users.noreply.github.com>
2020-10-02 10:08:52 -0400

    quick changes
    enlarged settings button and added to both pages

--- app/src/main/java/com/android/studyspot/MapFragment.java
+++ app/src/main/java/com/android/studyspot/MapFragment.java
@@ -1,5 +1,6 @@
 package com.android.studyspot;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,8 @@ public class MapFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private ImageButton settingsButton;
 
     public MapFragment() {
         // Required empty public constructor
@@ -56,6 +60,15 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View root =  inflater.inflate(R.layout.fragment_map, container, false);
+        settingsButton = (ImageButton) root.findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent settingsIntent = new Intent(getActivity().getApplicationContext(), SettingsActivity.class);
+                startActivity(settingsIntent);
+            }
+        });
+        return root;
     }
 }

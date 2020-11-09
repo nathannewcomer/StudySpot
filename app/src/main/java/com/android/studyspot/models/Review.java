bd5d2183b53fa43c957b6b365b546fb742d0b60d
AbidFAli-OSU <ali.655@osu.edu>
2020-11-09 16:36:54 -0500

    Added support for parceling Review objects

--- app/src/main/java/com/android/studyspot/models/Review.java
+++ app/src/main/java/com/android/studyspot/models/Review.java
@@ -1,6 +1,9 @@
 package com.android.studyspot.models;
 
-public class Review {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Review implements Parcelable {
     private double mRating;
     private int mLikes;
     private String mComment;
@@ -13,8 +16,8 @@ public class Review {
     public Review(){
         mRating = 0;
         mLikes = 0;
-        mComment = null;
-        mPhotoPath = null;
+        mComment = "";
+        mPhotoPath = "";
     }
     public Review(double rating, int likes, String comment, String photoPath){
         mRating = rating;
@@ -22,6 +25,26 @@ public class Review {
         mComment = comment;
         mPhotoPath = photoPath;
     }
+
+    protected Review(Parcel in) {
+        mRating = in.readDouble();
+        mLikes = in.readInt();
+        mComment = in.readString();
+        mPhotoPath = in.readString();
+    }
+
+    public static final Creator<Review> CREATOR = new Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
+
     public double getRating() {
         return mRating;
     }
@@ -54,5 +77,18 @@ public class Review {
 
     public void setPhotoPath(String photoPath) {
         mPhotoPath = photoPath;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeDouble(mRating);
+        dest.writeInt(mLikes);
+        dest.writeString(mComment);
+        dest.writeString(mPhotoPath);
     }
 }
